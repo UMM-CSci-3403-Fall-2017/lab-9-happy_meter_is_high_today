30b4969d938c3749d1cb4b209c1219463ccc82c5
root <root@DESKTOP-FK3SQFB.localdomain>
2017-12-10 12:48:03 -0600

    Added some code to help walk throught the code to write it out faster

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,27 @@
 package segmentedfilesystem;
 
-public class Main {
-    
-    public static void main(String[] args) {
-        
-    }
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
+
+public class Main {
+
+
+	public static void main(String[] args) throws IOException {
+		int portNumber = 6014;
+		InetAddress ipAddress = InetAddress.getByName("146.57.33.55");
+
+		DatagramSocket socket = new DatagramSocket();
+
+		//Pinging the server
+		byte[] upBuffer = new byte[1028];
+		DatagramPacket upPacket = new DatagramPacket(upBuffer, upBuffer.length, ipAddress, portNumber);
+		socket.send(upPacket);
+
+		PacketReceiver pack_receiver = new PacketReceiver();
+
+		pack_receiver.startReceivingPackets(socket);
+	}
 }
--- src/segmentedfilesystem/byteReceiver.java
+++ src/segmentedfilesystem/byteReceiver.java
@@ -1,1 +1,27 @@
 package segmentedfilesystem;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.unti.Comparator;
+
+
+public class byteReceiver{
+  //need to handle the differnt files
+
+  //need see what files I have
+
+  //store files are they are received from the sender
+
+  //Check what type of file the packet is
+
+  //On that note need to find the header
+
+  //Then the rest of the fils
+
+  //And finally the last packets needs to be handled, and the # number of
+  //pacekts that its supposed to haave reived before this one.
+
+
+}
